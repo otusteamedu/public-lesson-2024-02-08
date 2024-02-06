f0166a5e3117f7bbbfab2ced9391925575980461
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-02-06 23:28:16 +0300

    Deserialize Request into DTO and add validation

--- src/Controller/Api/PayForOrder/v1/Controller.php
+++ src/Controller/Api/PayForOrder/v1/Controller.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller\Api\PayForOrder\v1;
 
+use App\Controller\Api\PayForOrder\v1\Input\OrderPaymentData;
 use App\Entity\Order;
 use App\Exception\AcquiringException;
 use App\Service\OrderService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[AsController]
@@ -18,21 +19,14 @@ class Controller
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
         private readonly OrderService $orderService,
-    )
-    {
+    ) {
     }
 
     #[Route(path: '/api/pay-for-order/v1', methods: ['POST'])]
-    public function __invoke(Request $request): Response
+    public function __invoke(#[MapRequestPayload] OrderPaymentData $orderPaymentData): Response
     {
-        $orderId = (int)$request->request->get('orderId');
-        $sum = (int)$request->request->get('sum');
-        $cardNumber = $request->request->get('cardNumber');
-        $owner = $request->request->get('owner');
-        $cvv = $request->request->get('cvv');
-
-        /** @var Order $order */
-        $order = $this->entityManager->getRepository(Order::class)->find($orderId);
+        /** @var Order|null $order */
+        $order = $this->entityManager->getRepository(Order::class)->find($orderPaymentData->orderId);
         if ($order === null) {
             return new JsonResponse(['success' => false, 'message' => 'Order not found'], Response::HTTP_NOT_FOUND);
         }
@@ -41,12 +35,12 @@ class Controller
             return new JsonResponse(['success' => false, 'message' => 'Invalid order status'], Response::HTTP_BAD_REQUEST);
         }
 
-        if ($sum !== $order->getSum()) {
+        if ($orderPaymentData->sum !== $order->getSum()) {
             return new JsonResponse(['success' => false, 'message' => 'Wrong sum provided'], Response::HTTP_BAD_REQUEST);
         }
 
         try {
-            $this->orderService->makePayment($sum, $cardNumber, $owner, $cvv);
+            $this->orderService->makePayment($orderPaymentData->sum, $orderPaymentData->cardNumber, $orderPaymentData->owner, $orderPaymentData->cvv);
         } catch (AcquiringException $e) {
             return new JsonResponse(['success' => false, 'message' => $e->getMessage()], Response::HTTP_UNPROCESSABLE_ENTITY);
         }
---
+++ src/Controller/Api/PayForOrder/v1/Input/OrderPaymentData.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Api\PayForOrder\v1\Input;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class OrderPaymentData
+{
+    public function __construct(
+        public readonly int $orderId,
+        public readonly int $sum,
+        #[Assert\Length(16)]
+        public readonly string $cardNumber,
+        #[Assert\NotEqualTo('Fake')]
+        public readonly string $owner,
+        #[Assert\Length(3)]
+        public readonly string $cvv,
+    ) {
+    }
+}
